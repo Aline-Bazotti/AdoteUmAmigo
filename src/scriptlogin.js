fa20c29befc64a5b26ab2aadba98e003ed34742a
Patrícia Steffen <patricia_steffen99@hotmail.com>
2019-11-01 21:31:26 -0300

    Login

--- src/scriptlogin.js
+++ src/scriptlogin.js
@@ -3,10 +3,10 @@ const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+	container.classList.add("rightlogin-panellogin-activelogin");
 });
 
 signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+	container.classList.remove("rightlogin-panellogin-activelogin");
 });
 
